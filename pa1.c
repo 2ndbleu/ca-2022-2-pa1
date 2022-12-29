1c778acb823b3041acc9d94ed4f04e2041a80533
2ndbleu <34599727+2ndbleu@users.noreply.github.com>
2022-12-30 00:21:48 +0900

    Update pa1.c
    TEST 0 OK!
    TEST 1 OK!
    TEST 2 OK!
    TEST 3 OK!
    TEST 4 OK!
    TEST 5 OK!
    TEST 6 OK!
    TEST 7 OK!
    TEST 8 OK!
    TEST 9 OK!
    TEST 10 OK!
    TEST 11 OK!
    TEST 12 OK!
    TEST 13 OK!
    TEST 14 OK!
    TEST 15 OK!
    TEST 16 OK!
    TEST 17 OK!
    TEST 18 OK!
    total: 190/190

--- pa1.c
+++ pa1.c
@@ -20,12 +20,109 @@
 typedef unsigned char u8;
 
 /* TODO: Implement this function */
-int encode(const u8* src, int width, int height, u8* result)
-{
+inline void bitwrite(u8* dst, int idx, u8 val, int len)
+{ // assuming len > 0
+    int idx1, idx2;
+    u8 mask1 = 0xff, mask2 = 0xff;
+    idx2 = idx + len, idx1 = idx2 & 0x7ffffff8;
 
-
-
-
-  return 0;
+    if (idx1 == idx2)
+    { // 1 byte simple
+        mask1 >>= (8 - len);
+        *(dst + (idx >> 3)) &= ~mask1; 
+        *(dst + (idx >> 3)) |= val & mask1;
+    }
+    else if (idx1 > idx)
+    { // 2 byte
+        // byte #N
+        mask1 >>= (idx & 7);
+        *(dst + (idx >> 3)) &= ~mask1;
+        *(dst + (idx >> 3)) |= (val >> (idx2 & 7)) & mask1;
+        // byte #N+1
+        mask2 <<= (8 - (idx2 & 7));
+        *(dst + (idx2 >> 3)) &= ~mask2;
+        *(dst + (idx2 >> 3)) |= (val << (8 - (idx2 & 7))) & mask2;
+    }
+    else
+    { // 1 byte complex
+        mask1 >>= (idx & 7);
+        mask2 >>= (idx2 & 7);
+        mask1 &= ~mask2;
+        *(dst + (idx >> 3)) &= ~mask1;
+        *(dst + (idx >> 3)) |= (val << (8 - (idx2 & 7))) & mask1;
+    }
 }
 
+inline u8 filter(const u8* src, int width, int i, int j) {
+    int avg = 0;
+
+    if (i == 0)
+        if (j == 0) return *src;
+        else return *(src + j) - *(src + j-1);
+    else if (j == 0)
+        return *(src + i * width) - *(src + (i-1) * width);
+    else
+    {
+        avg = (int)*(src + (i-1) * width + (j-1))
+        + *(src + (i-1) * width + j)
+        + *(src + i * width + (j-1));
+        avg /= 3;
+
+        return *(src + i * width + j) - (u8)avg;
+    }
+}
+
+int encode(const u8 *src, int width, int height, u8 *result)
+{
+    // bitindex, base, max
+    int idx = 0;
+    u8 base = *src, max = base, delta;
+    int n_bits;
+
+    if (width == 0 || height == 0)
+        return 0;
+
+    for (int i = 0; i < height; i++)
+    {
+        base = 0xff, max = 0x00;
+        // phase 1
+        for (int j = 0; j < width; j++)
+        {
+            delta = filter(src, width, i, j);
+            base = base > delta ? delta : base;
+            max = max < delta ? delta : max;
+        }
+        // phase 2 : calculate n_bits
+        delta = max - base;
+        if (delta & 0x80) n_bits = 8;
+        else if (delta & 0x40) n_bits = 7;
+        else if (delta & 0x20) n_bits = 6;
+        else if (delta & 0x10) n_bits = 5;
+        else if (delta & 0x08) n_bits = 4;
+        else if (delta & 0x04) n_bits = 3;
+        else if (delta & 0x02) n_bits = 2;
+        else if (delta & 0x01) n_bits = 1;
+        else n_bits = 0;
+        // base : 8 bits
+        bitwrite(result, idx, base, 8);
+        idx += 8;
+        // n_bits : 4 bits
+        bitwrite(result, idx, n_bits, 4);
+        idx += 4;
+        // deltas : n(i) bits x width
+        if (n_bits != 0)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                delta = filter(src, width, i, j) - base;
+                bitwrite(result, idx, delta, n_bits);
+                idx += n_bits;
+            }
+        }
+    }
+    // padding
+    while (idx & 7)
+        bitwrite(result, idx++, 0, 1);
+    
+    return idx >> 3;
+}
